ebc8a57c43fcd76f29f9f342e4c736e96325f619
Léo SENIQUE <leo.senique@gmail.com>
2020-08-24 02:59:00 +0200

    fix CORS manquant et ajout script

--- articles-front/src/environments/environment.prod.ts
+++ articles-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://articles-back:8080/articles'
+  backendUrl: 'http://localhost:9000/articles'
 };
---
+++ docker-compose.sh
@@ -0,0 +1,6 @@
+cd articles-back
+mvn clean package -Dmaven.test.skip
+cd ../articles-front
+npm run build -- --prod
+cd ..
+docker-compose up --build
